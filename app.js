f8dc3d91198f0395c3e7fffaa9c0b0caad33342c
David Odeyinka <david.odeyinka@yahoo.com>
2019-01-25 16:36:24 +0100

    Yet another change

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ var path = require('path');
 // var jade = require('jade');
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.listen(3000);
+console.log('Server is running on port 3000...')
