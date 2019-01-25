cbff3338803b7bf93ee65dc5098db7ad5d9bbb38
David Odeyinka <david.odeyinka@yahoo.com>
2019-01-25 17:41:37 +0100

    Yet another change

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.set('view engine', 'jade');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', function(req, res) {
 	// console.log('Start!');
